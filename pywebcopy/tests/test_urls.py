5fdb7edefdeab201c5224dc0a8a4f5d7631f12ca
Raja Tomar <rajatomar788@gmail.com>
2022-04-20 09:37:16 -0700

    v7.0.2.3
    1. Expanded the testing on various platforms using github workflows.
    2. Fixed a secure_filename testcase on linux platforms.
    
    Signed-off-by: Raja Tomar <rajatomar788@gmail.com>

--- pywebcopy/tests/test_urls.py
+++ pywebcopy/tests/test_urls.py
@@ -141,4 +141,7 @@ class TestUrl2Path(unittest.TestCase):
         for i in ["NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8",
                   "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]:
             with self.subTest(i=i):
-                self.assertEqual(secure_filename(i), '_' + i)
+                if os.name == 'nt':
+                    self.assertEqual(secure_filename(i), '_' + i)
+                else:
+                    self.assertEqual(secure_filename(i), i)
